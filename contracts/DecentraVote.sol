5ea500d6fc017f8aa7c3fadb1531faa8845477b2
Bravo1B9 <brofe93@gmail.com>
2023-04-18 15:00:07 +0000

    Initial smart contract

---
+++ contracts/DecentraVote.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract DecentraVote {
+
+    struct Candidate {
+        address candidateAddr;
+        string name;
+        string campaignDesc;
+    }
+
+    error CandidateAlreadyExists();
+
+    mapping(address => Candidate) public candidates;
+
+    function registerAsCandidate(
+        address _addr,
+        string memory _name,
+        string memory _campaignDesc
+    ) external {
+        if(candidates[_addr].candidateAddr == _addr) {
+            revert CandidateAlreadyExists();
+        }
+        Candidate memory newCandidate;
+        newCandidate.candidateAddr = _addr;
+        newCandidate.name = _name;
+        newCandidate.campaignDesc = _campaignDesc;
+        candidates[_addr] = newCandidate;
+    }
+
+}
