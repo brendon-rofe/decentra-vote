50052448461dca320fc9c7c7daece8d014f9c728
Brendon Rofe <b1@xgmi.com>
2023-04-19 13:53:15 +0200

    Initial data structures setup

--- contracts/DecentraVote.sol
+++ contracts/DecentraVote.sol
@@ -3,6 +3,28 @@ pragma solidity ^0.8.19;
 
 contract DecentraVote {
 
-    
+    struct Campaign {
+        uint id;
+        string name;
+        string description;
+        bool isActive;
+        uint start;
+        uint end;
+        mapping(uint => Candidate) candidates;
+        mapping(address => bool) hasVoted;
+        uint candidateCount;
+        uint totalVotes;
+    }
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    struct Voter {
+        address id;
+        string votedFor;
+    }
 
 }
