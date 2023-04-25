351fc1f811a976a2935b103dcaad3e5944eae21a
Brendon Rofe <b1@xgmi.com>
2023-04-25 07:57:48 +0200

    Initial hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4", // Specify the Solidity version you want to use
+  networks: {
+    hardhat: {
+      chainId: 1337 // Set the chainId to avoid MetaMask issues during testing
+    },
+  },
+};
